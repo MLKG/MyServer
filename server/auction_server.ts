1b61de2d7f7b6f4dff31d02a86153096b9ee45e6
曾桂林 <zengguilin@pywm.com.cn>
2017-11-30 23:16:15 +0800

    第八章：第4节课 webSocket通讯

--- server/auction_server.ts
+++ server/auction_server.ts
@@ -1,4 +1,6 @@
 import * as express from 'express';
+import { Server } from "ws";
+
 const app = express();
 export class Product {
   constructor(
@@ -33,4 +35,12 @@ app.get('/api/products/:id', (req, res) => {
 
 const server = app.listen(8000, "localhost", () => {
   console.log("服务器已经启动，地址是：http://localhost:8000");
+});
+
+const wsServer = new Server({port: 8085});
+wsServer.on('connection', (websocket) => {
+  websocket.send("这个消息是服务器主动推送的");
+  websocket.on("message", (message) => {
+    console.log('接收到消息' + message);
+  })
 });
